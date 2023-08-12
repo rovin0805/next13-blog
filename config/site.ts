5bf273b817abd2477df0915e1119b2eb1c93291a
Choi Eun Jeong <qhdrn67@naver.com>
2023-08-12 13:28:46 +0900

    feat : site config

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+const siteConfig = {
+  siteName: "Explorer",
+  description:
+    "A minimal and lovely travel blog which shares experiences and cities around the world!",
+  currentlyAt: "Seoul",
+  socialLinks: {
+    github: "https://www.github.com/rovin0805",
+  },
+};
+
+export default siteConfig;
